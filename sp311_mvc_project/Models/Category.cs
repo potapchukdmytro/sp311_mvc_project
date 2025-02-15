e80a1e30a0799ad1616fe5989d1b1df57b84bffd
potapchukdmytro <traiget@gmail.com>
2025-02-15 13:15:04 +0200

    Add project files.

---
+++ sp311_mvc_project/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sp311_mvc_project.Models
+{
+    public class Category
+    {
+        [Key]
+        public string? Id { get; set; }
+        [Required, MaxLength(100)]
+        public string? Name { get; set; }
+
+        public List<Product> Products { get; set; } = [];
+    }
+}
